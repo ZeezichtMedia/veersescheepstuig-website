7fd92d716b8b09678af2358d6ddd1c6f08ce2ae8
ZeezichtMedia <info@zee-zicht.nl>
2026-02-26 17:24:31 +0100

    chore: remove prebuilt .vercel artifacts from git

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/fotos-videos_UI07DS9c.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./fotos-videos_BtVuy2_O.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
